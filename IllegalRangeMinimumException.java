db34d6b8b3c7525c681be440a3349f3decfbf3cf
vedi0boy <vedioboy07@gmail.com>
2017-06-02 15:52:53 -0400

    Added serial version uid for exceptions.

--- IllegalRangeMinimumException.java
+++ IllegalRangeMinimumException.java
@@ -4,7 +4,12 @@ package net.hollowbit.vedi0boy.rangeindexer;
  * Created by Nathanael on 6/2/2017.
  */
 public class IllegalRangeMinimumException extends IllegalArgumentException {
-
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
     public IllegalRangeMinimumException() {
         super("The specified range minimum is illegal. It must be less than the first range limit.");
     }
